ff26681bd0bb1594a07c2306b5862e873b0fac85
Aisha <aishyped@gmail.com>
2025-02-08 09:16:03 +0530

    Delete set 2 directory

--- set 2/pracex.py
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <time.h>
-void main (void)
-{ 
-    time_t curent_time = time (NULL);
-    char date_string[20];
-    strftime(date_string,20,"%Y-%m-%d",localtime(&curent_time));
-    printf("the curent dateis:%s\n",date_string);
-}
